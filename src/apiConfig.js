2b37acb9f52704d89d05ea9129258f4e8f0caf45
tkeatkaew <thanit@rmutl.ac.th`>
2024-02-09 10:44:06 +0700

    config files added

---
+++ src/apiConfig.js
@@ -0,0 +1,10 @@
+export const apiConfig = { 
+    development: {
+        bearer_token : '1234567890',
+        API_URL : 'http://localhost:4000',
+    },
+    production: {
+        bearer_token : '1234567890',
+        API_URL : 'https://api.se-rmutl.net',
+    }
+};
--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,10 +1,18 @@
+
+import { env } from '../env.js';
+import { apiConfig } from '../apiConfig.js';
+
 export async function searchMovie(search_data) {
 
     try {
-
         console.log('search_data: ' + search_data);
-
-        const response = await fetch('https://api.se-rmutl.net/api/movie/search?search_text=' + search_data);
+        const response = await fetch(apiConfig[env].API_URL + '/api/movie/search?search_text=' + search_data,
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${apiConfig[env].bearer_token}`,
+                }
+            },);
 
         return await response.json(); //***
 
@@ -17,21 +25,22 @@ export async function searchMovie(search_data) {
 }
 
 export async function getAllMovies() {
-    var bearer_token = '1234567890';
-    
+
     try {
 
+        console.log('env myConfig: ' + env);
+        console.log('apiConfig.bearer_token: ' + apiConfig[env].bearer_token);
+        console.log('apiConfig.API_URL: ' + apiConfig[env].API_URL);
+
         //const response = await fetch('http://localhost:4000/api/movie/all',
-        const response = await fetch('https://api.se-rmutl.net/api/movie/all',
+        const response = await fetch(apiConfig[env].API_URL + '/api/movie/all',
             {
                 method: "GET",
                 headers: {
-                    Authorization: `Bearer ${bearer_token}`,
+                    Authorization: `Bearer ${apiConfig[env].bearer_token}`,
                 }
             },);
         return await response.json();
-
-
 
     } catch (error) {
         return [];
@@ -40,15 +49,24 @@ export async function getAllMovies() {
 }
 
 export async function createMovie(data) {
-    const response = await fetch(`https://api.se-rmutl.net/api/movie/insert`, {
-        //mode: 'no-cors',
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
+    try {
 
-    })
-    console.log('Create Movie response: ' + await response);
+        const response = await fetch(apiConfig[env].API_URL + '/api/movie/insert', {
+            //mode: 'no-cors',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${apiConfig[env].bearer_token}`,
+            },
+            body: JSON.stringify(data)
 
-    // return  response.json();
-    return response;
+        })
+        console.log('Create Movie response: ' + await response);
+
+        // return  response.json();
+        return response;
+        
+    } catch (error) {
+        return [];
+    }
 }
